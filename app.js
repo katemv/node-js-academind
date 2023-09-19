b74c75b5fed6109202f5db60d7e67fe659cf8254
katemv <mvkatt@gmail.com>
2023-09-19 10:57:53 -0400

    add pug

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
+app.use((req, res) => {
    res.status(404)
-       .sendFile(path.join(rootDir, "views", "404.html"));
+       // .sendFile(path.join(rootDir, "views", "404.html"));
+       .render("404", { docTitle: "404 | Page not found" })
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const path = require("path");
-
-const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
-router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir, "views", "add-product.html"))
+router.get("/add-product", (req, res) => {
+    // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+    res.render("add-product", { docTitle: "Add Product"});
 });
 
-router.post("/add-product", (req, res, next) => {
-    products.push({ title : req.body.title })
+router.post("/add-product", (req, res) => {
+    products.push({ title : req.body.title });
     res.redirect("/");
 });
 
