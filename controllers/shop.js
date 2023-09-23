017ec4b834227024c4ba457fd2c37fe72a1591e4
katemv <mvkatt@gmail.com>
2023-09-23 10:27:38 -0400

    add sequelize & product model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/old_product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res) => {
--- util/database.js
+++ util/database.js
@@ -1,10 +1,13 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "node-complete",
-    password: "rootpass"
-});
+const sequelize = new Sequelize(
+    "node-complete",
+    "root",
+    "rootpass",
+    {
+        dialect: "mysql",
+        host: "localhost"
+    }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
