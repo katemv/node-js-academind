63fd1dbb4f8ae698813ade793dac16c919a588c3
katemv <mvkatt@gmail.com>
2023-10-16 16:56:49 -0400

    add route protection

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -14,21 +15,21 @@ router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 
 // /cart => GET
-router.get("/cart", shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
 // /cart => POST
-router.post("/cart", shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
 // /cart-delete-item => POST
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
 // /orders => GET
-router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
 // /create-order => GET
-router.get("/create-order", shopController.postOrder);
+router.get("/create-order", isAuth, shopController.postOrder);
 
 // /checkout => GET
-router.get("/checkout", shopController.getCheckout);
+router.get("/checkout", isAuth, shopController.getCheckout);
 
 module.exports = router;
